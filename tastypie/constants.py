1d4cbf09521ba5c2aa0934b17544f279d77c55e3
risent <shengqi542@gmail.com>
2017-12-23 16:29:40 +0800

    Add QUERY_TERMS constants

--- tastypie/constants.py
+++ tastypie/constants.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 ALL = 1
 # Enable all ORM filters, including across relationships
 ALL_WITH_RELATIONS = 2
+
+# Valid query types (a set is used for speedy lookups). These are (currently)
+# considered SQL-specific; other storage systems may choose to use different
+# lookup types.
+QUERY_TERMS = {
+    'exact', 'iexact', 'contains', 'icontains', 'gt', 'gte', 'lt', 'lte', 'in',
+    'startswith', 'istartswith', 'endswith', 'iendswith', 'range', 'year',
+    'month', 'day', 'week_day', 'hour', 'minute', 'second', 'isnull', 'search',
+    'regex', 'iregex',
+}
